2b3e08a07a63fc44c86885c485715c9c2ea1f7ea
allancalderwood <acalde202@caledonian.ac.uk>
2020-02-12 16:16:06 +0000

    Initial setup of flutter app

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart'; // material design components
+
+void main() => runApp(RedditReader()); // run the application
+
+class RedditReader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+
+}
+
